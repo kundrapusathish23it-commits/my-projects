f7a8fdbc0e90bd6d582b4ebe46dc6526ee4154f1
kundrapusathish23it-commits <kundrapusathish.23.it@anits.edu.in>
2026-02-07 14:17:58 +0530

    Set fixed number for factorial calculation
    Replaced user input with a fixed number for factorial calculation.

--- factorial.java
+++ factorial.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class factorial {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        
         System.out.print("Enter number: ");
-        int n = sc.nextInt();
+        int n = 5;
         int fact = 1;
 
         for(int i=1;i<=n;i++){
